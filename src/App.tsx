33beb643c46368bbb8d8c7bacd5ccee59fc2c186
Chun-gu <chungu.yi92@gmail.com>
2022-02-18 23:57:48 +0900

    ✨ Inputs 컴포넌트 렌더

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RecoilRoot } from "recoil";
+import Inputs from "./components/Inputs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>환율 계산기</header>
+      <RecoilRoot>
+        <Inputs />
+      </RecoilRoot>
+    </>
   );
 }
 
